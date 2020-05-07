ad6d1ee8aca56069b915f5091dd1772cc1496eaa
Lorenzo <lorenzopappalardo7@gmail.com>
2020-05-07 17:00:22 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/it/geek/pd2buildmanager/MainActivity.kt
@@ -0,0 +1,6 @@
+package it.geek.pd2buildmanager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appTheme = ThemeData(
+  accentColor: Colors.blueAccent,
+  backgroundColor: Colors.blueGrey,
+  bottomAppBarColor: Colors.blue[900],
+  buttonColor: Colors.blueAccent,
+  cardColor: Colors.blueGrey,
+  cursorColor: Colors.blueAccent,
+  disabledColor: Colors.blueGrey,
+  primaryColor: Colors.blueAccent,
+  primaryColorDark: Colors.blueAccent[200],
+);
